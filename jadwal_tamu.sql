34fb5f0b315332dd6d837797a6671f3d37e9d4b6
Andri Firman Saputra <65483768+hako-975@users.noreply.github.com>
2023-06-07 16:47:21 +0700

    UPDATE DB

--- jadwal_tamu.sql
+++ jadwal_tamu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Jun 2023 pada 11.57
+-- Waktu pembuatan: 07 Jun 2023 pada 11.46
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 8.1.1
 
@@ -31,15 +31,16 @@ CREATE TABLE `jadwal` (
   `id_jadwal` int(11) NOT NULL,
   `id_tamu` int(11) NOT NULL,
   `jadwal_tamu` datetime NOT NULL,
-  `tujuan` text NOT NULL
+  `tujuan` text NOT NULL,
+  `status` enum('BELUM','SUDAH') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `id_tamu`, `jadwal_tamu`, `tujuan`) VALUES
-(4, 3, '2023-06-06 16:48:00', 'asd123');
+INSERT INTO `jadwal` (`id_jadwal`, `id_tamu`, `jadwal_tamu`, `tujuan`, `status`) VALUES
+(7, 1, '2023-06-07 16:43:00', 'Bertemu Bos', 'SUDAH');
 
 -- --------------------------------------------------------
 
@@ -59,8 +60,7 @@ CREATE TABLE `tamu` (
 --
 
 INSERT INTO `tamu` (`id_tamu`, `nama_tamu`, `alamat_tamu`, `no_telp_tamu`) VALUES
-(1, 'andri123', 'pocis123', '087808675313123'),
-(3, 'asd123', '123', 'asds');
+(1, 'Andri Firman Saputra', 'Jl. AMD Babakan Pocis', '087808675313');
 
 -- --------------------------------------------------------
 
@@ -113,7 +113,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `tamu`
